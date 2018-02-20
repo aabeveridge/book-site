9a23f31b6597d4a10e31816861ac9653c3454845
aabeveridge <akbeveri@uncg.edu>
2018-02-20 14:49:07 -0500

    updates

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,11 @@
-$('#previewModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-});
-
 $("#navtoggle").click(function(){
     $("i").toggleClass("fa-bars fa-close");
 });
+
+$('#responseauthors').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+});
+
+$(window).on('load',function(){
+        $('#myModal').modal('show');
+    });
